7483ebd3eac5313d181c667457b7aa103742381b
BM. Zahidul Islam <bmzahidulislam@outlook.com>
2022-03-26 11:29:05 +0600

    font awesome icon added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,15 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 
 const Product = ({ productItems }) => {
     const { img, name, price } = productItems;
     return (
         <div>
-            <div className="card" style={{ width: "18rem" }}>
-                <img src="..." className="card-img-top" alt="..." />
+            <div className="card h-100" style={{ width: "18rem" }}>
+                <img src={img} className="card-img-top rounded" alt="..." />
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#/" className="btn btn-primary">Go somewhere</a>
+                    <h5 className="card-title">{name}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Price : $ {price}</h6>
+                    <button class="btn btn-primary w-100" type="button">Add to Cart <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon></button>
                 </div>
             </div>
         </div>
